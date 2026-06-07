db4a6ce2968a73f3f91d8a7c831a5a1a034d4254
Pedro <pedrocarvi@outlook.com>
2026-06-07 16:07:37 -0300

    corrijo api url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 //const BASE_URL = 'https://gym-backend-dev-rust.vercel.app'
-// const BASE_URL = 'https://saddlebrown-okapi-551688.hostingersite.com'
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://saddlebrown-okapi-551688.hostingersite.com'
+// const BASE_URL = 'http://localhost:3000';
 // const BASE_URL = 'https://gym-backend-rust.vercel.app';
 // const BASE_URL = 'https://backend-agentrenamiento.vercel.app';
 // 
