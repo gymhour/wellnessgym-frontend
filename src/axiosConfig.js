807c33edd9a318b3d861e97c0a62034b98ba2dd2
gymhour <gymhourmails@gmail.com>
2026-06-12 18:10:57 -0300

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,7 @@ import { jwtDecode } from 'jwt-decode';
 // API URL LOCAL
 // const BASE_URL = 'http://localhost:3000';
 // API URL PROD
-const BASE_URL = 'https://cornflowerblue-lapwing-349229.hostingersite.com';
+const BASE_URL = 'https://wellnessgym-backend.vercel.app';
 
 // Cliente para login / register / cambiar contraseña (sin interceptores)
 export const authClient = axios.create({
